2a6ad21be7c575aaf47cdaea734aeea3ec4c066a
Govind S <govindsys1008@gmail.com>
2026-04-17 08:18:13 +0530

    switched to uvicorn server

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,6 +77,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'core.wsgi.application'
+ASGI_APPLICATION='core.asgi.application'
 
 
 # Database
